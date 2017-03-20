7ff6ff238ba45ea59d173db27b81e662da9d5f3d
Liichy Aguilar Bueno <Liichy Aguilar Bueno>
2017-03-20 15:10:12 -0500

    Interacción juego

--- js/main.js
+++ js/main.js
@@ -1,4 +1,75 @@
 $(document).ready(init);
+var turno ="x";
+var turno2="o";
+var bandera=0;
+var ban=false;
+var opciones=new Array(9);
+
+function caremoco(){
+    // random
+    oli=localStorage.getItem("nombre_1",jugador1);
+    chaujj=localStorage.getItem("nombre_2",jugador2);
+
+    var moco=Math.floor((Math.random()*2));
+    bandera = moco;
+    if(moco==0){
+        document.getElementById("div_turno").innerHTML="turno del jugador "+A;
+    }else{
+        document.getElementById("div_turno").innerHTML="turno del jugador "+ B;
+    }
+
+}
+
+function marcar (id)
+{   
+    var celda =document.getElementById(id);
+    console.log(opciones[id]);
+    if (bandera == 0 && !opciones[id])
+    {
+           celda.value=turno;
+           document.getElementById("div_turno").innerHTML="turno del jugador "+ B;
+           opciones[id]=1;
+            bandera = 1;
+           
+    }else if(bandera == 1 && !opciones[id]){
+           
+           celda.value=turno2;
+           document.getElementById("div_turno").innerHTML="turno del jugador "+ A;
+           opciones[id]=0;
+        bandera = 0;
+    }
+
+    jugadorx(1);
+    jugadorx(0);
+}
+
+function jugadorx(_option){
+   if (opciones[1]==_option && opciones [2]==_option && opciones[3]==_option){
+       //alert("felicidades jugador ganaste" +turno+"ganaste");
+       swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
+   }else if (opciones[4]==_option && opciones [5]==_option && opciones[6]==_option){
+        swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
+   }else if (opciones[7]==_option && opciones [8]==_option && opciones[9]==_option){
+         swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
+   }else if (opciones[1]==_option && opciones [5]==_option &&opciones[9]==_option){
+       swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
+   }else if (opciones[3]==_option && opciones [5]==_option &&opciones[7]==_option){
+         swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
+   }else if (opciones[1]==_option && opciones [4]==_option &&opciones[7]==_option){
+       swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
+   }else if (opciones[2]==_option && opciones [5]==_option &&opciones[8]==_option){
+         swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
+   }else if (opciones[3]==_option && opciones [6]==_option &&opciones[9]==_option){
+        swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
+   }
+}
+
+   
+function reiniciar(){
+    
+   location.reload();
+   
+} 
 
 var currentSection = null;
 var currentGameID;
@@ -23,7 +94,6 @@ function onClickBtnItemJuego()
 	gotoSection('historial-detalle');
 	getComentarios(idGame);
 	currentGameID = idGame;
-	//getSingleGame(idGame);
 }
 
 function onClickBtnSaludo() {
@@ -44,9 +114,6 @@ function onClickBtnComentar()
 	enviarComentario(currentGameID, $('#name').val(), $('#content').val());
 }
 
-
-
-
 function enviarComentario(_idGame, _name, _content)
 {
 	$.ajax({
@@ -62,9 +129,7 @@ function enviarComentario(_idGame, _name, _content)
 function gotoSection(_identificadorDeSeccion) {
 	currentSection.removeClass('visible');
 	var nextSection = $('#' + _identificadorDeSeccion);
-
 	nextSection.addClass('visible');
-
 	TweenMax.from(nextSection, 1.5, {scale: 0.2, opacity: 0, ease: Elastic.easeOut});
 	currentSection = nextSection;
 }
@@ -115,83 +180,8 @@ function dibujarHistorial(_datos) {
 
 	for (var i in _datos) {
 		console.log(_datos[i].winner_player);
-
 		var html = '<li data-idgame="'+ _datos[i].id +'" class="list-group-item">' + _datos[i].winner_player + ' le gano a '+ _datos[i].loser_player +' en ' + _datos[i].number_of_turns_to_win + ' movimientos <button class="btn">Comentar</button></li>';
 		lista.append(html);
 	}
 }
 
-
-
-
-var turno ="x";
-var turno2="o";
-var bandera=0;
-var ban=false;
-var opciones=new Array(9);
-
-function caremoco(){
-    // random
-    oli=localStorage.getItem("nombre_1",jugador1);
-    chaujj=localStorage.getItem("nombre_2",jugador2);
-
-    var moco=Math.floor((Math.random()*2));
-    bandera = moco;
-    if(moco==0){
-        document.getElementById("div_turno").innerHTML="turno del jugador "+;
-    }else{
-        document.getElementById("div_turno").innerHTML="turno del jugador "+;
-    }
-    /*console.log(moco);*/
-}
-
-function marcar (id)
-{   
-    var celda =document.getElementById(id);
-    console.log(opciones[id]);
-    if (bandera == 0 && !opciones[id])
-    {
-           celda.value=turno;
-           document.getElementById("div_turno").innerHTML="turno del jugador "+;
-           opciones[id]=1;
-            bandera = 1;
-           
-    }else if(bandera == 1 && !opciones[id]){
-           
-           celda.value=turno2;
-           document.getElementById("div_turno").innerHTML="turno del jugador "+;
-           opciones[id]=0;
-        bandera = 0;
-    }
-    //bandera++;
-    jugadorx(1);
-    jugadorx(0);
-}
-
-function jugadorx(_option){
-   if (opciones[1]==_option && opciones [2]==_option && opciones[3]==_option){
-       //alert("felicidades jugador ganaste" +turno+"ganaste");
-       swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
-   }else if (opciones[4]==_option && opciones [5]==_option && opciones[6]==_option){
-        swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
-   }else if (opciones[7]==_option && opciones [8]==_option && opciones[9]==_option){
-         swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
-   }else if (opciones[1]==_option && opciones [5]==_option &&opciones[9]==_option){
-       swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
-   }else if (opciones[3]==_option && opciones [5]==_option &&opciones[7]==_option){
-         swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
-   }else if (opciones[1]==_option && opciones [4]==_option &&opciones[7]==_option){
-       swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
-   }else if (opciones[2]==_option && opciones [5]==_option &&opciones[8]==_option){
-         swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
-   }else if (opciones[3]==_option && opciones [6]==_option &&opciones[9]==_option){
-        swal({title: "Ganaste..!",text: "felicidades jugador " +turno+ " ganaste",imageUrl: "img/ganaste.png"});
-   }
-}
-
-   
-function reiniciar(){
-    
-   location.reload();
-   
-} 
